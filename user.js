99b2c3a50e6604df8926b9d03a45990557b22e24
sn99 <42946112+sn99@users.noreply.github.com>
2023-01-08 15:03:10 +0530

    user.js

---
+++ user.js
@@ -0,0 +1,10 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.newtabpage.activity-stream.newNewtabExperience.enabled", false);
+
+user_pref("layout.frame_rate", 120);
+
+user_pref("dom.ipc.processCount", 12);
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+
+user_pref("extensions.pocket.enabled", false);
